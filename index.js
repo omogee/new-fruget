d3018e7d7b70aed52c59944f73eac69bf315ec33
MAC <mac@MACs-MacBook-Pro.local>
2023-03-10 11:48:00 +0100

    fresh

--- index.js
+++ index.js
@@ -420,10 +420,10 @@ app.get(`/fetch_messages`, (req, res) =>{
 })
 })
 if(process.env.NODE_ENV === "production"){ 
-  app.use(express.static("ogbmain/build"))
+  app.use(express.static("frontend/build"))
   
   app.get('*',(req,res)=>{
-      res.sendFile(path.join(__dirname, 'ogbmain','build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
   })
   }  
 app.get("/", (req, res)=>{
